34660dcbca3e66d6c705bcb26e34b55a6a99cf49
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-10-06 15:06:13 -0500

    ex8

---
+++ ex8.js
@@ -0,0 +1,19 @@
+const dayNames = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const tempsF = [60, 65, 70, 68, 72, 78, 80];
+
+function convertFtoC(tempF) {
+  return (tempF - 32) * (5 / 9);
+}
+
+/* Write code to print out the temperature in Celsius for each day:
+   `${dayName} it will be ${tempC} degrees Celsius`
+*/
