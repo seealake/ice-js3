41a040378d3e1465e9951214c63c45726368e0a7
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-10-06 10:49:57 -0500

    adding some examples and a debug

---
+++ ex1.js
@@ -0,0 +1,18 @@
+const student = {
+  name: "Bucky Badger",
+  age: 21,
+  isFullTime: true,
+};
+
+/* WITHOUT EDITING THE ABOVE CODE,
+   change the data so that the final console.log prints your own information
+   (or some that you make up)
+*/
+
+// write code here
+
+console.log(student);
+
+/* save the file and execute in terminal with:
+   node ex1.js
+*/
---
+++ ex2.js
@@ -0,0 +1,19 @@
+const student = {
+  name: "Bucky Badger",
+  age: 21,
+  isFullTime: true,
+};
+
+const newStudent = {};
+
+/* WITHOUT EDITING, COPYING, or PASTING the above code,
+   change the data so that the final console.log prints Bucky's information
+*/
+
+// write code here
+
+console.log(newStudent);
+
+/* save the file and execute in terminal with:
+   node ex2.js
+*/
