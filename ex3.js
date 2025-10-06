1837cab284ec17922affcb80217472862fb39070
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-10-06 10:53:57 -0500

    ex3

---
+++ ex3.js
@@ -0,0 +1,20 @@
+const student = {
+  name: "Bucky Badger",
+  age: 21,
+  isFullTime: true,
+};
+
+const key = "isFullTime";
+
+/* WITHOUT EDITING, COPYING, or PASTING the above code,
+   WITHOUT writing any new dot-notation or string literals,
+   change the data so that the final console.log prints false
+*/
+
+// write code here
+
+console.log(student.isFullTime);
+
+/* save the file and execute in terminal with:
+   node ex3.js
+*/
